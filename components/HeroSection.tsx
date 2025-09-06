2f6ad212bcf5e9f968052a6c148409124565db92
AshrafChitamba <bsc-40-20@unima.ac.mw>
2025-09-06 17:41:36 +0200

    style - hero_section

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,12 +1,20 @@
 "use client";
 
 import React from "react";
-import {} from 'react-hook-form'
+import {} from "react-hook-form";
 
 export const HeroSection = () => {
   return (
-    <section className="h-screen ">
-      <img src="/supermarket.jpg" alt="supermarket" className="h-full w-full object-cover" />
+    <section className="bg-[#f0fafb] px-8 py-4">
+      <div className="relative h-[580px] w-full rounded-[10px] overflow-hidden">
+        <img
+          src="/hero-banner.jpg"
+          alt="supermarket"
+          className="h-full w-full object-cover"
+        />
+
+        <div className="absolute inset-0 bg-gradient-to-r from-[#01333499] from-60% to-[#d9d9d900]"></div>
+      </div>
     </section>
   );
 };
