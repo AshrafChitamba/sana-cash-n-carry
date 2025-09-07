6130095899820652f7e415265ab1a69391fc8da5
AshrafChitamba <bsc-40-20@unima.ac.mw>
2025-09-07 11:51:05 +0200

    style - TextInputField

---
+++ components/TextInputField.tsx
@@ -0,0 +1,56 @@
+import { FC, HTMLInputTypeAttribute } from "react";
+import { FieldError, UseFormRegisterReturn } from "react-hook-form";
+
+type TextInputFieldProps = {
+  type?: HTMLInputTypeAttribute | "textarea";
+  id: string;
+  placeholder?: string;
+  disabled: boolean;
+  label?: string;
+  registerProps: UseFormRegisterReturn;
+  error: FieldError | undefined;
+};
+
+export const TextInputField: FC<TextInputFieldProps> = (props) => {
+  return (
+    <div>
+      {props.label ? (
+        <label
+          htmlFor={props.id}
+          className="block text-sm font-medium text-header-text mb-1.5 text-start"
+        >
+          {props.label}
+        </label>
+      ) : null}
+      <div className="relative">
+        {props.type === "textarea" ? (
+          <textarea
+            id={props.id}
+            rows={5}
+            placeholder={props.placeholder}
+            disabled={props.disabled}
+            className="w-full bg-white px-4 py-3 border rounded-[10px] focus:border-primary outline-hidden"
+            autoComplete="true"
+            {...props.registerProps}
+          />
+        ) : (
+          <input
+            type={props.type || "text"}
+            id={props.id}
+            placeholder={props.placeholder}
+            disabled={props.disabled}
+            className="w-full bg-white px-4 py-3 border rounded-[10px] focus:border-primary outline-hidden"
+            autoComplete="true"
+            {...props.registerProps}
+          />
+        )}
+
+        {props.error && (
+          <p className="mt-1 text-sm text-red-600 text-start!">
+            {props.error.message ?? ""}
+          </p>
+        )}
+      </div>
+    </div>
+  );
+};
