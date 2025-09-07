eaa0285afa9604f337cc3152834686b6e65b2ba1
AshrafChitamba <bsc-40-20@unima.ac.mw>
2025-09-07 05:50:25 +0200

    style - categories_section

---
+++ data/categories.ts
@@ -0,0 +1,14 @@
+export const categories = [
+  { name: "Furniture", image: "/furniture.jpg" },
+  { name: "Hardware", image: "/hardware.jpg" },
+  { name: "Groceries", image: "/groceries.jpg" },
+  { name: "Clothes", image: "/clothes.jpg" },
+  { name: "Catering", image: "/catering.jpg" },
+  { name: "Catering", image: "/furniture.jpg" },
+  { name: "Electronics", image: "/furniture.jpg" },
+  { name: "Catering", image: "/furniture.jpg" },
+  { name: "Baby Store", image: "/babies.png" },
+  { name: "Furniture", image: "/furniture.jpg" },
+  { name: "Hospitality", image: "/furniture.jpg" },
+  { name: "Kitchenware", image: "/kitchenware.jpg" },
+];
