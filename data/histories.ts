87d6a5558b67812b4643e662675f712b36a4ac3c
AshrafChitamba <bsc-40-20@unima.ac.mw>
2025-09-07 06:22:03 +0200

    chore - histories_data

---
+++ data/histories.ts
@@ -0,0 +1,36 @@
+type HistoryType = {
+  date: string;
+  title: string;
+  description: string;
+};
+
+type HistorySectionType = {
+  image: string;
+  slideHistories: [HistoryType?, HistoryType?];
+};
+
+export const histories: HistorySectionType[] = [
+  {
+    image: "/",
+    slideHistories: [
+      {
+        date: "24/06/1997",
+        title: "Company Registration",
+        description: `Sana Cash & Carry is a chain of supermarkets in 
+            Malawi with locations in Lilongwe and Blantyre. They offer a
+            wide selection of fresh produce, groceries, and household goods,
+            with competitive pricing. They also provide online shopping and
+            delivery services through their website.`,
+      },
+      {
+        date: "14/05/2015",
+        title: "20+ Stores",
+        description: `Sana Cash & Carry is a large retail company with 
+            multiple supermarket locations across Malawi, including in
+            Lilongwe, Blantyre, and Mzuzu. It is known for offering a wide
+            selection of groceries, household items, fresh produce, and
+            other goods at competitive prices.`,
+      },
+    ],
+  },
+];
