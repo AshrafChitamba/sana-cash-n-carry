ea3e1bfccc87c37958ea030fb1ff99f51efb8826
AshrafChitamba <bsc-40-20@unima.ac.mw>
2025-09-07 17:19:00 +0200

    BigClientsSection_section_carousel

--- components/BigClientsSection.tsx
+++ components/BigClientsSection.tsx
@@ -1,20 +1,34 @@
+"use client"
+
 import { bigClients } from "@/data/bigClients";
-import React from "react";
+import { useCarouselAutoPlay } from "@/hooks/useCarouselAutoPlay";
+import { motion } from "motion/react";
+import { useRef } from "react";
 
 export const BigClientsSection = () => {
+  const carouselContainerRef = useRef<HTMLDivElement>(null);
+  const { horizontalPosition, revealingItems } = useCarouselAutoPlay<string>({
+    carouselContainerRef,
+    items: bigClients,
+  });
+
   return (
-    <section className="bg-white py-7 px-8">
-      <div className="grid grid-cols-5">
-        {bigClients.map((clientLogo, index) => (
-          <div key={index} className="flex items-center justify-center">
+    <div className="bg-white py-7 px-8 overflow-hidden w-full">
+      <motion.div
+        ref={carouselContainerRef}
+        className="grid grid-flow-col justify-center auto-cols-[200px] gap-8"
+        style={{ x: horizontalPosition }}
+      >
+        {revealingItems.map((logoUrl, itemIndex) => (
+          <div key={itemIndex} className="flex items-center justify-center">
             <img
-              src={clientLogo}
-              alt={"Client_Logo"}
+              src={logoUrl}
+              alt={`Client logo ${itemIndex + 1}`}
               className="w-24 h-auto"
             />
           </div>
         ))}
-      </div>
-    </section>
+      </motion.div>
+    </div>
   );
 };
