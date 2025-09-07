2c74eab94fc5e91a10f12bfd65271806216ffb59
AshrafChitamba <bsc-40-20@unima.ac.mw>
2025-09-07 10:59:05 +0200

    style - footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,89 @@
 export const Footer = () => {
-  return <div></div>;
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer className="text-white">
+      <div className="bg-primary py-10 px-8 grid grid-cols-4 justify-items-center">
+        <div>
+          <img src="/logo.png" alt="SANA_LOGO" className="object-contain" />
+          <div className="mt-3">
+            <h4 className="font-semibold text-2xl mb-3">
+              Sona Cash &apos;n&apos; Carry Retail <br /> Shopping at retail
+              price
+            </h4>
+            <p className="text-sm">Malawi leading supermarket</p>
+          </div>
+        </div>
+        <div>
+          <h4 className="font-semibold text-xl mb-2">Quick Links</h4>
+          <ul className="flex flex-col items-start gap-2 text-[#FCFCFC]">
+            <li>
+              <a href="#" className="text-sm">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-sm">
+                Our Categories
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-sm">
+                Our History
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-sm">
+                Contact Us
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-sm">
+                Find a store
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h4 className="font-semibold text-xl mb-2">Get In Touch</h4>
+          <ul className="flex flex-col items-start gap-2 text-[#FCFCFC]">
+            <li>
+              <a href="#" className="text-sm">
+                (+265) 988 88 88 20
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-sm">
+                Instagram
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-sm">
+                Facebook
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h4 className="font-semibold text-xl mb-2">Legal</h4>
+          <ul className="flex flex-col items-start gap-2 text-[#FCFCFC]">
+            <li>
+              <a href="#" className="text-sm">
+                Privacy policy
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-sm">
+                Terms of service
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <small className="block bg-[#013334] py-3.5 px-8 text-center">
+        © {currentYear} Sana Cash &apos;n&apos; Carry. All right reserved
+      </small>
+    </footer>
+  );
 };
