4fad6fc45c3007876118ad5d216d2fe1f13a06bc
AshrafChitamba <bsc-40-20@unima.ac.mw>
2025-09-08 17:11:33 +0200

    style - socialLinks

--- components/Header/SocialLinks.tsx
+++ components/Header/SocialLinks.tsx
@@ -10,23 +10,29 @@ export const SocialLinks = ({ isMobile }: { isMobile: boolean }) => {
     <div
       className={clsx("flex items-center", isMobile ? "gap-x-3" : "gap-x-2.5")}
     >
-      <a href="#" target="_blank" rel="noopener noreferrer">
-        <FaFacebookF
-          size={isMobile ? MOBILE_ICON_SIZE : DESKTOP_ICONS_SIZE}
-          className="text-secondary"
-        />
+      <a
+        href="#"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-secondary hover:text-primary transition-colors"
+      >
+        <FaFacebookF size={isMobile ? MOBILE_ICON_SIZE : DESKTOP_ICONS_SIZE} />
       </a>
-      <a href="#" target="_blank" rel="noopener noreferrer">
-        <FaInstagram
-          size={isMobile ? MOBILE_ICON_SIZE : DESKTOP_ICONS_SIZE}
-          className="text-secondary"
-        />
+      <a
+        href="#"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-secondary hover:text-primary transition-colors"
+      >
+        <FaInstagram size={isMobile ? MOBILE_ICON_SIZE : DESKTOP_ICONS_SIZE} />
       </a>
-      <a href="#" target="_blank" rel="noopener noreferrer">
-        <FaLinkedinIn
-          size={isMobile ? MOBILE_ICON_SIZE : DESKTOP_ICONS_SIZE}
-          className="text-secondary"
-        />
+      <a
+        href="#"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-secondary hover:text-primary transition-colors"
+      >
+        <FaLinkedinIn size={isMobile ? MOBILE_ICON_SIZE : DESKTOP_ICONS_SIZE} />
       </a>
     </div>
   );
